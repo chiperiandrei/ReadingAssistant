00ebb293a1bacf55699c09a8e43514e0b9b41e49
stioana79 <ioanadana97@gmail.com>
2020-03-31 23:00:43 +0300

    added backend

---
+++ backend/xml-parsing.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+import geograpy3
+
+myTree = ET.parse('MappingBooks_standoff.xml')
+myRoot = myTree.getroot()
+
+proposition = []
+finalText = {}
+nr_prop = 1
+
+for y in myRoot.findall(".//W[@ID]"):
+    word = 'w' + str(nr_prop) + '.'
+    if word in y.attrib['ID']:
+        proposition.append(y.attrib['text'])
+    else:
+        finalText['p.' + str(nr_prop)] = ' '.join(proposition)
+        proposition = [y.attrib['text']]
+        nr_prop = nr_prop + 1
+
+# Pentru a concatena si ultima fraza
+finalText['p.' + str(nr_prop)] = ' '.join(proposition)
+
+for key in sorted(finalText, key=lambda j: int(j.split('p.')[1])):
+    # print(finalText[key])
+    more_places = geograpy3.get_place_context(text=finalText[key])
+    print(more_places.cities)
+    print(more_places.regions)
+    print(more_places.places)
