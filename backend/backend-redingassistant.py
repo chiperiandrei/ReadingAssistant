ad9751e2e6462b5ef0886ac812dc5d93c16d36d8
stioana79 <ioanadana97@gmail.com>
2020-03-31 23:44:46 +0300

    created server

--- backend/backend-redingassistant.py
+++ backend/backend-redingassistant.py
@@ -4,6 +4,9 @@ import geograpy3
 myTree = ET.parse('MappingBooks_standoff.xml')
 myRoot = myTree.getroot()
 
+my_latitude = 47.154798
+my_longitude = 27.605946
+
 proposition = []
 finalText = {}
 nr_prop = 1
@@ -20,9 +23,18 @@ for y in myRoot.findall(".//W[@ID]"):
 # Pentru a concatena si ultima fraza
 finalText['p.' + str(nr_prop)] = ' '.join(proposition)
 
+places_from_text = []
+
 for key in sorted(finalText, key=lambda j: int(j.split('p.')[1])):
     # print(finalText[key])
     more_places = geograpy3.get_place_context(text=finalText[key])
-    print(more_places.cities)
-    print(more_places.regions)
-    print(more_places.places)
+    places_from_text.append(more_places.cities)
+    places_from_text.append(more_places.regions)
+    places_from_text.append(more_places.places)
+
+print(places_from_text)
+
+######
+
+earthRadius = 6372.8
+
