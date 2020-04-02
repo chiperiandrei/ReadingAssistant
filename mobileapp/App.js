a1b2e2d6abeafff4e47ac7c4196d02dd45a14c6d
Andrei Chiperi <andrei.chiperi98@gmail.com>
2020-04-02 15:16:44 +0300

    DoD week7

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,19 +1,29 @@
+/* eslint-disable react/no-did-mount-set-state */
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   Alert,
-  Button,
   SafeAreaView,
-  StatusBar,
   StyleSheet,
   Switch,
   Text,
   View,
+  ImageBackground,
 } from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import { Header, Button, Avatar } from 'react-native-elements';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Geolocation from '@react-native-community/geolocation';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import Sound from 'react-native-sound';
+
+import Tts from 'react-native-tts';
+
+const image = {
+  uri:
+    'https://i.pinimg.com/236x/15/f9/00/15f9007cc00d7f54dec4ba5ab8dd5816.jpg',
+};
 
 class App extends React.Component {
   constructor() {
@@ -31,14 +41,14 @@ class App extends React.Component {
     Geolocation.getCurrentPosition(
       position => {
         const initialPosition = JSON.stringify(position);
-        this.setState({initialPosition});
+        this.setState({ initialPosition });
       },
       error => Alert.alert('Error', JSON.stringify(error)),
-      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
     );
     this.watchID = Geolocation.watchPosition(position => {
       const lastPosition = JSON.stringify(position);
-      this.setState({lastPosition});
+      this.setState({ lastPosition });
     });
 
     const sound = new Sound(require('./music.mp3'), '', error => {
@@ -46,7 +56,6 @@ class App extends React.Component {
         Alert.alert('error', error.message);
       }
     });
-
     this.setState({
       sound,
     });
@@ -64,15 +73,22 @@ class App extends React.Component {
   _stop = () => {
     this.state.sound.stop();
   };
+  _playTTS = () => {
+    Tts.setDefaultLanguage('ro-RO');
+    Tts.speak(
+      'Vasile Alecsandri (n. 21 iulie S.N. 2 august 1821, undeva în ținutul Bacăului, Moldova — d. 22 august S.N. 3 septembrie 1890, Mircești, județul Roman, România) a fost un poet, dramaturg, folclorist, om politic, ministru, diplomat, membru fondator al Academiei Române, creator al teatrului românesc și al literaturii dramatice în România, personalitate marcantă a Moldovei și apoi a României de-a lungul întregului secol al XIX-lea.',
+    );
+  };
+  _stopTTS = () => {
+    Tts.stop();
+  };
 
   _toggleLoop = value => {
-    console.warn(value);
     if (value) {
       this.state.sound.setNumberOfLoops(-1); // loop infinite
     } else {
       this.state.sound.setNumberOfLoops(0); // no loop
     }
-
     this.setState({
       isLoop: value,
     });
@@ -85,57 +101,104 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <Text style={styles.sectionTitle}>
-            TILN PROJECT - READING ASSISTANT
-          </Text>
-          <View>
-            <Text>
-              <Text style={styles.title}>Initial position: </Text>
-              {this.state.initialPosition}
-            </Text>
-            <Text>
-              <Text style={styles.title}>Current position: </Text>
-              {this.state.lastPosition}
-            </Text>
-          </View>
-        </SafeAreaView>
-        <SafeAreaView
-          style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Button title={'Play'} onPress={this._play} />
-          <Button title={'Pause'} onPress={this._pause} />
-          <Button title={'Stop'} onPress={this._stop} />
-          <Text style={styles.highlight}>Repeat?</Text>
-          <Switch
-            onValueChange={value => this._toggleLoop(value)}
-            value={this.state.isLoop}
+        <ImageBackground source={require('./assets/img/background.jpg')} style={styles.image}>
+
+          <Header
+            leftComponent={<Avatar
+              size="medium"
+              rounded
+              icon={{ name: 'directions-walk', type: 'material' }}
+
+            />}
+            centerComponent={{ text: 'READING ASSISTANT', style: { color: '#fff', fontSize: 23, fontWeight: 'bold' } }}
+            rightComponent={
+              <Avatar
+                size="medium"
+                rounded
+                icon={{ name: 'book', type: 'font-awesome' }}
+              />}
           />
-        </SafeAreaView>
+          <View>
+            <Text style={styles.textWhite}>Initial position: </Text>
+            <Text style={styles.textWhite}>{this.state.initialPosition}</Text>
+            <Text style={styles.textWhite}>Current position: </Text>
+            <Text style={styles.textWhite}>{this.state.lastPosition}</Text>
+          </View>
+          <View style={{ display: 'flex', flexDirection: 'column' }}>
+            <Button
+              icon={
+                <Icon
+                  raised
+                  name='play'
+                  type='font-awesome' />
+              }
+              title="Play Sound"
+
+              onPress={this._play} />
+            <Button
+              icon={
+                <Icon
+                  raised
+                  name='pause'
+                  type='font-awesome' />
+              }
+              title="Pause Sound"
+
+              onPress={this._pause} />
+            <Button
+              icon={
+                <Icon
+                  raised
+                  name='stop'
+                  type='font-awesome' />
+              }
+              title="Pause Sound"
+
+              onPress={this._stop} />
+
+            <Button
+              icon={
+                <Icon
+                  raised
+                  name='microphone'
+                  type='font-awesome' />
+              }
+              title="Play TTS"
+
+              onPress={this._playTTS} />
+            <Button
+              icon={
+                <Icon
+                  raised
+                  name='microphone-slash'
+                  type='font-awesome' />
+              }
+              title="Stop TTS"
+
+              onPress={this._stopTTS} />
+            <Text style={styles.highlight}>Repeat?</Text>
+            <Switch
+              onValueChange={value => this._toggleLoop(value)}
+              value={this.state.isLoop}
+            />
+          </View>
+        </ImageBackground>
       </>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
   engine: {
     position: 'absolute',
     right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
   },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  textWhite: {
+    color: Colors.white,
   },
   sectionDescription: {
     marginTop: 8,
@@ -153,6 +216,10 @@ const styles = StyleSheet.create({
     padding: 4,
     paddingRight: 12,
     textAlign: 'right',
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
   },
 });
 
