7df0245ab0c61e4897fcc494c7713c1c762b4ea0
stioana79 <ioanadana97@gmail.com>
2020-04-02 13:59:42 +0300

    calculate distance

--- backend/backend-redingassistant.py
+++ backend/backend-redingassistant.py
@@ -1,15 +1,11 @@
 import xml.etree.ElementTree as ET
 import geograpy3
-
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
-
+import math
 
 myTree = ET.parse('MappingBooks_standoff.xml')
 myRoot = myTree.getroot()
-
-my_latitude = 47.154798
-my_longitude = 27.605946
 
 proposition = []
 finalText = {}
@@ -23,12 +19,11 @@ for y in myRoot.findall(".//W[@ID]"):
         finalText['p.' + str(nr_prop)] = ' '.join(proposition)
         proposition = [y.attrib['text']]
         nr_prop = nr_prop + 1
-
-# Pentru a concatena si ultima fraza
+# pentru a concatena si ultima fraza
 finalText['p.' + str(nr_prop)] = ' '.join(proposition)
 
-places_from_text = []
 
+places_from_text = []
 for key in sorted(finalText, key=lambda j: int(j.split('p.')[1])):
     # print(finalText[key])
     more_places = geograpy3.get_place_context(text=finalText[key])
@@ -39,10 +34,8 @@ for key in sorted(finalText, key=lambda j: int(j.split('p.')[1])):
     print(more_places.regions)
     print(more_places.places)
 
-# print(places_from_text)
 
-
-# aici calculez lat si lon pentru fiecare place din places_from_text
+# aici calculez lat si long pentru fiecare place din places_from_text
 geolocator = Nominatim()
 lat_lon = []
 for place in places_from_text:
@@ -52,9 +45,26 @@ for place in places_from_text:
             print(location.latitude, location.longitude)
             lat_lon.append(location)
     except GeocoderTimedOut as e:
-        print("Error: geocode failed on input %s with message %s", (place, e))
-
+        print("Nu a fost gasita o locatie")
 print(lat_lon)
 
-# earthRadius = 6372.8
 
+# aici calculez locatiile din apropiere
+def haversine(coord1, coord2):
+    R = 6372800  # Earth radius in meters
+    lat1, lon1 = coord1
+    lat2, lon2 = coord2
+    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+    dphi = math.radians(lat2 - lat1)
+    dlambda = math.radians(lon2 - lon1)
+    a = math.sin(dphi / 2) ** 2 + \
+        math.cos(phi1) * math.cos(phi2) * math.sin(dlambda / 2) ** 2
+    return 2 * R * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+
+my_actual_location = 47.154798, 27.605946
+nearby_places = []
+# calculate distance between actual location and locations from text
+for city, coord in places_from_text:
+    distance = haversine(my_actual_location, coord)
+    print(city, distance)
