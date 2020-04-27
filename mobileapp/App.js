5304c7142abd4a92fad23483062736d0eef9444d
Andrei Chiperi <andrei.chiperi98@gmail.com>
2020-04-27 15:57:32 +0300

    functional compoennet

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,9 +1,6 @@
-/* eslint-disable react/no-did-mount-set-state */
-/* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Alert,
-  SafeAreaView,
   StyleSheet,
   Switch,
   Text,
@@ -13,181 +10,168 @@ import {
 import { Header, Button, Avatar } from 'react-native-elements';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Geolocation from '@react-native-community/geolocation';
-
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 import Sound from 'react-native-sound';
-
 import Tts from 'react-native-tts';
+import Spinner from 'react-native-loading-spinner-overlay';
 
-const image = {
-  uri:
-    'https://i.pinimg.com/236x/15/f9/00/15f9007cc00d7f54dec4ba5ab8dd5816.jpg',
+const soundStyle = {
+  display: 'flex',
+  flexDirection: 'column',
 };
+const App = props => {
+  const [lastPosition, setLastPosition] = useState(null);
+  const [initialPosition, setInitialPosition] = useState(null);
+  const [sound, setSound] = useState({});
+  const [isLoop, setIsLoop] = useState(false);
+  const [spinner, setSpinner] = useState(true);
 
-class App extends React.Component {
-  constructor() {
-    super();
-    Sound.setCategory('Playback', true); // true = mixWithOthers
-    this.state = {
-      lastPosition: null,
-      initialPosition: null,
-      sound: {},
-      isLoop: false,
-    };
-  }
-
-  componentDidMount() {
-    Geolocation.getCurrentPosition(
-      position => {
-        const initialPosition = JSON.stringify(position);
-        this.setState({ initialPosition });
-      },
-      error => Alert.alert('Error', JSON.stringify(error)),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
-    );
-    this.watchID = Geolocation.watchPosition(position => {
-      const lastPosition = JSON.stringify(position);
-      this.setState({ lastPosition });
-    });
-
-    const sound = new Sound(require('./music.mp3'), '', error => {
-      if (error) {
-        Alert.alert('error', error.message);
-      }
-    });
-    this.setState({
-      sound,
-    });
-  }
-
-  _play = () => {
-    this.state.sound.play(() => {
-      this.state.sound.release();
+  const play = () => {
+    sound.play(() => {
+      sound.release();
     });
   };
-  _pause = () => {
-    this.state.sound.pause();
+  const pause = () => {
+    sound.pause();
+  };
+  const stop = () => {
+    sound.stop();
   };
 
-  _stop = () => {
-    this.state.sound.stop();
-  };
-  _playTTS = () => {
+  const _playTTS = () => {
     Tts.setDefaultLanguage('ro-RO');
     Tts.speak(
       'Vasile Alecsandri (n. 21 iulie S.N. 2 august 1821, undeva în ținutul Bacăului, Moldova — d. 22 august S.N. 3 septembrie 1890, Mircești, județul Roman, România) a fost un poet, dramaturg, folclorist, om politic, ministru, diplomat, membru fondator al Academiei Române, creator al teatrului românesc și al literaturii dramatice în România, personalitate marcantă a Moldovei și apoi a României de-a lungul întregului secol al XIX-lea.',
     );
   };
-  _stopTTS = () => {
+  const _stopTTS = () => {
     Tts.stop();
   };
 
-  _toggleLoop = value => {
+  const _toggleLoop = value => {
     if (value) {
-      this.state.sound.setNumberOfLoops(-1); // loop infinite
+      sound.setNumberOfLoops(-1); // loop infinite
     } else {
-      this.state.sound.setNumberOfLoops(0); // no loop
+      sound.setNumberOfLoops(0); // no loop
     }
-    this.setState({
-      isLoop: value,
-    });
+    setIsLoop(value);
   };
 
-  componentWillUnmount() {
-    this.watchID != null && Geolocation.clearWatch(this.watchID);
-  }
+  useEffect(() => {
+    const watchID = Geolocation.getCurrentPosition(
+      position => {
+        const initPosition = JSON.stringify(position);
+        setInitialPosition(initPosition);
+        setSpinner(false);
+      },
+      error => console.log(error),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+    Geolocation.watchPosition(position => {
+      const lastPos = JSON.stringify(position);
+      setLastPosition(lastPos);
+    });
+    const soundObj = new Sound(require('./music.mp3'), '', error => {
+      if (error) {
+        Alert.alert('error', error.message);
+      }
+    });
+    setSound(soundObj);
+    return function cleanup() {
+      Geolocation.clearWatch(watchID)
+    }
+  }, []);
 
-  render() {
+  if (spinner === true) {
     return (
       <>
-        <ImageBackground source={require('./assets/img/background.jpg')} style={styles.image}>
-
+        <ImageBackground
+          source={require('./assets/img/background.jpg')}
+          style={styles.image}>
+          <View>
+            <Spinner
+              visible={spinner}
+              textContent={'Getting your location...'}
+              textStyle={styles.spinnerTextStyle}
+              color={'cyan'}
+              size={'large'}
+            />
+          </View>
+        </ImageBackground>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <ImageBackground
+          source={require('./assets/img/background.jpg')}
+          style={styles.image}>
           <Header
-            leftComponent={<Avatar
-              size="medium"
-              rounded
-              icon={{ name: 'directions-walk', type: 'material' }}
-
-            />}
-            centerComponent={{ text: 'READING ASSISTANT', style: { color: '#fff', fontSize: 23, fontWeight: 'bold' } }}
+            leftComponent={
+              <Avatar
+                size="medium"
+                rounded
+                icon={{ name: 'directions-walk', type: 'material' }}
+              />
+            }
+            centerComponent={{
+              text: 'READING ASSISTANT',
+              style: { color: '#fff', fontSize: 23, fontWeight: 'bold' },
+            }}
             rightComponent={
               <Avatar
                 size="medium"
                 rounded
                 icon={{ name: 'book', type: 'font-awesome' }}
-              />}
+              />
+            }
           />
           <View>
             <Text style={styles.textWhite}>Initial position: </Text>
-            <Text style={styles.textWhite}>{this.state.initialPosition}</Text>
+            <Text style={styles.textWhite}>{initialPosition}</Text>
             <Text style={styles.textWhite}>Current position: </Text>
-            <Text style={styles.textWhite}>{this.state.lastPosition}</Text>
+            <Text style={styles.textWhite}>{lastPosition}</Text>
           </View>
-          <View style={{ display: 'flex', flexDirection: 'column' }}>
+          <View style={soundStyle}>
             <Button
-              icon={
-                <Icon
-                  raised
-                  name='play'
-                  type='font-awesome' />
-              }
+              icon={<Icon raised name="play" type="font-awesome" />}
               title="Play Sound"
-
-              onPress={this._play} />
+              onPress={play}
+            />
             <Button
-              icon={
-                <Icon
-                  raised
-                  name='pause'
-                  type='font-awesome' />
-              }
+              icon={<Icon raised name="pause" type="font-awesome" />}
               title="Pause Sound"
-
-              onPress={this._pause} />
+              onPress={pause}
+            />
             <Button
-              icon={
-                <Icon
-                  raised
-                  name='stop'
-                  type='font-awesome' />
-              }
-              title="Pause Sound"
-
-              onPress={this._stop} />
+              icon={<Icon raised name="stop" type="font-awesome" />}
+              title="Stop Sound"
+              onPress={stop}
+            />
 
             <Button
-              icon={
-                <Icon
-                  raised
-                  name='microphone'
-                  type='font-awesome' />
-              }
+              icon={<Icon raised name="microphone" type="font-awesome" />}
               title="Play TTS"
-
-              onPress={this._playTTS} />
+              onPress={_playTTS}
+            />
             <Button
-              icon={
-                <Icon
-                  raised
-                  name='microphone-slash'
-                  type='font-awesome' />
-              }
+              icon={<Icon raised name="microphone-slash" type="font-awesome" />}
               title="Stop TTS"
-
-              onPress={this._stopTTS} />
+              onPress={_stopTTS}
+            />
             <Text style={styles.highlight}>Repeat?</Text>
             <Switch
-              onValueChange={value => this._toggleLoop(value)}
-              value={this.state.isLoop}
+              onValueChange={value => _toggleLoop(value)}
+              value={isLoop}
             />
           </View>
         </ImageBackground>
       </>
     );
   }
-}
+};
 
+export default App;
 const styles = StyleSheet.create({
   engine: {
     position: 'absolute',
@@ -222,5 +206,3 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
   },
 });
-
-export default App;
